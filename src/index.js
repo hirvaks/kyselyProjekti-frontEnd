21f9a81a7cfe2f7c85588058bd69f78c64531ef2
Akseli Hirvelä <akseli.hirvela@gmail.com>
2022-04-12 17:21:30 +0300

    index.js tiedostoon lisätty Linkit kehityksen ajaksi + koodin siistiminen

--- src/index.js
+++ src/index.js
@@ -2,38 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Kysely1 from './Kysely1';
 import Vastaus1 from './Vastaus1';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, BrowserRouter, Link} from 'react-router-dom';
 import {KyselyListElement} from './KyselyTesti';
 
-
-
-
+// Linkit lisätty kehityksen ajaksi helpon navigoinnin vuoksi
 
 ReactDOM.render(
   <React.StrictMode>
-    
-
     <BrowserRouter>
-    <Routes>
-        <Route path="/">
-          
-          <Route index path="Kysely1" element={<Kysely1  />} />
-          <Route path="Vastaus1" element={<Vastaus1 />} />
-          <Route path="KyselyTesti" element={<KyselyListElement />} />
 
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          
-        </Route>
+      <Link to="/">Home "/"</Link>{' '} <br/>
+      <Link to="Kysely1">Kysely 1</Link>{' '} <br/>
+      <Link to="KyselyTesti">Kyselytesti</Link>{' '}
+
+      <Routes>
+        <Route index path="Kysely1" element={<Kysely1  />} />
+        <Route path="Vastaus1" element={<Vastaus1 />} />
+        <Route path="KyselyTesti" element={<KyselyListElement />} />
       </Routes>
 
-    </BrowserRouter>
+      {/* Using path="*"" means "match anything", so this route
+          acts like a catch-all for URLs that we don't have explicit
+          routes for. */}
 
-    
-    
+    </BrowserRouter>
   </React.StrictMode>,
-  
   
   document.getElementById('root')
 );
